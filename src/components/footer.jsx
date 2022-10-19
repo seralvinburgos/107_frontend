90ba1b4752f5c75ba07352f775cfbf4393f0b045
Seralvin Burgos <seralvin.burgos@gmail.com>
2022-10-18 21:51:40 -0400

    needing to figure out how to add icons from fontawesome

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,15 +1,27 @@
 import "./footer.css";
+import { FontAwesomeIcon } from '@fortawesome/react-native/font-awesome'
+import { solid, regular, brands, icon } from '@fortawesome/fontawesome-svg-core/import.macro'
+
 
 import {Link} from 'react-router-dom';
 
 
 function Footer() {
     return (
-        <div className="footer fixed-bottom">
-        <div>2022</div>
-        <div className="name">
-        </div>
-    </div>
+        <ul class="nav justify-content-center">
+  <li class="nav-item">
+    <Link class="nav-link active" aria-current="page" href="#">Active</Link>
+  </li>
+  <li class="nav-item">
+  <FontAwesomeIcon icon={brands('facebook')} />
+  </li>
+  <li class="nav-item">
+    <Link class="nav-link" to="#"></Link>
+  </li>
+  <li class="nav-item">
+    <Link class="nav-link disabled">Disabled</Link>
+  </li>
+</ul>
 
 
     );
