02f5c572e44cd3d18688eff13eb70527d2c22195
Seralvin Burgos <seralvin.burgos@gmail.com>
2022-10-20 21:57:20 -0400

    updates to home title animations and carousel

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -12,37 +12,48 @@ import { motion } from "framer-motion";
       className="container-fluid">
          <div className="top_main container-fluid text-center mt-3">
                 <div className="d-flex flex-row admin-top mx-auto">
-                    <div className="top_1 position-relative col-12 shadow rounded-top">
-                        <img src="./images/back10.jpg" className="donut_back img-fluid" alt="back3" />
+                    <motion.div 
+                    className="top_1 position-relative col-12 shadow">
+                        <img
+                        src="./images/back10.jpg" className="donut_back img-fluid" alt="back3" />
                             <div className="top_2 position-absolute top-0 start-0 mt-5 ms-5">
                                 <motion.p 
-                                animate={{ scale: 1 }}
-                                initial={{ scale: 0.8 }}
+                                initial={{ y: -200 }}
+                                animate={{ 
+                                 scale: 1,
+                                 y: 0,
+                              }}
                                 transition={{
                                  type: "spring",
+                                 stiffness: 90,
                                  delay: 1
-                                 
                                }}
                                 className="title d-inline-flex" id="donutTitle">Donut  <span style={{'--i':'1'}}>S</span><span style={{'--i':'2'}}>H</span><span style={{'--i':'3'}}>O</span><span style={{'--i':'4'}}>P</span>
                                 </motion.p>
                             </div>
-                            <div className="top_3 position-absolute bottom-50 start-50 ms-5 mt-4">
+                            <motion.div 
+                            initial={{ y: -400 }}
+                            animate={{
+                              scale: 1,
+                              y: 0
+                            }}
+                              transition={{
+                                 type: "spring",
+                                 stiffness: 100,
+                                 delay: 1
+                              }}
+                            className="top_3 position-absolute bottom-50 start-50 ms-5 mt-4">
                                 <p className="title">Freshly Made Everyday</p>
-                            </div>
-                    </div>
+                            </motion.div>
+                    </motion.div>
                 </div>
          </div>
          <div className="aboutUs">
             <div className="text-center mt-3">
                <div className="threePic container-fluid d-inline-flex flex-row justify-content-center mx-auto position-relative gap-1">
-                  <div className="leftPic col-4 shadow rounded">
-                     <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="true">
-                        <div className="carousel-indicators">
-                           <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                           <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                           <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                        </div>
-                        <div className="carousel-inner">
+                  <div className="leftPic col-4 shadow">
+                  <div id="carouselExampleFade" className="carousel slide carousel-fade" data-bs-ride="carousel">
+                     <div className="carousel-inner">
                         <div className="carousel-item active">
                            <img src="./images/back1.jpg" className="d-block w-100" alt="..." />
                         </div>
@@ -50,20 +61,20 @@ import { motion } from "framer-motion";
                            <img src="./images/back2.jpg" className="d-block w-100" alt="..." />
                         </div>
                         <div className="carousel-item">
-                           <img src="./images/stack3.jpg" className="d-block w-100" alt="..." />
+                           <img src="./images/back3.jpg" className="d-block w-100" alt="..." />
                         </div>
                      </div>
-                     <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                     <span className="visually-hidden">Previous</span>
+                     <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Previous</span>
                      </button>
-                     <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                     <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                     <span className="visually-hidden">Next</span>
+                     <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Next</span>
                      </button>
                      </div>
                   </div>
-                  <div className="middlePic col-3 mx-5 shadow rounded-bottom">
+                  <div className="middlePic col-3 mx-5 shadow">
                      <div className="middleAbout">
                         <h3>About Us</h3>
                      </div>
