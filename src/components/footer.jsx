44b4906973a83c3026aa5de2a70baba9c3053329
Seralvin Burgos <seralvin.burgos@gmail.com>
2022-10-21 22:28:06 -0400

    adjusted height of footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,23 +4,25 @@ import "./footer.css";
 
 function Footer() {
     return (
-        <div className="footer fixed-bottom container-fluid d-flex flex-row">
-            <div className="left_side">
-                <h6>quick links</h6>
-                <p>Catalog</p>
-                <p>Wish List</p>
-                <p>About</p>
+        <div className="footer fixed-bottom container-fluid">
+            <div className="left_sid d-flex flex-row gap-4">
+                <li>Home</li>
+                <li>Catalog</li>
+                <li>Wish List</li>
+                <li>About</li>
             </div>
-            <div className="middle-sec">
+            <div className="middle-sec d-flex flex-row gap-4">
+                <div className="signSect">
                 <h6>sign up and save</h6>
                 <p>Subscribe for special offers and deals.</p>
+                </div>
                 <div className="subscribe form-floating mb-3">
                     <input type="email" className="form-control subInput" id="floatingInput" placeholder="name@example.com" />
                     <label for="floatingInput">Email address</label>
                 </div>
             </div>
             <div className="name">
-                <p>Alvin Burgos</p>
+                <li>Alvin Burgos</li>
             </div>
         </div>
 
